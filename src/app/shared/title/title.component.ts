42f92cd5cbb86161c0d6d4108e36277f273153d0
Carol <carolsanchez071@gmail.com>
2024-02-23 00:03:07 -0400

    Control flow for-if-switch-input

--- src/app/shared/title/title.component.ts
+++ src/app/shared/title/title.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input, booleanAttribute } from '@angular/core';
 
 @Component({
   selector: 'app-title',
   standalone: true,
-  imports: [],
-  templateUrl: './title.component.html',
-  styles: ``
+  imports: [CommonModule],
+  template: `
+    <h3 class="font-bold text-3xl pt-10 pb-10"> {{title}}</h3>
+  `
 })
 export class TitleComponent {
-
+  @Input({required:true}) title!: string;
+  @Input({transform: booleanAttribute}) withShadow:boolean = false;
 }
