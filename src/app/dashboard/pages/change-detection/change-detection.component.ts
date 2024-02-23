eb2f5be1da0e4a09400f7d05ab64b347f4a24555
Carol <carolsanchez071@gmail.com>
2024-02-23 12:48:14 -0400

    Change Detection

--- src/app/dashboard/pages/change-detection/change-detection.component.ts
+++ src/app/dashboard/pages/change-detection/change-detection.component.ts
@@ -1,12 +1,40 @@
+import { ChangeDetectionStrategy, Component, computed, signal } from '@angular/core';
+import { TitleComponent } from '../../../shared/title/title.component';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
 
 @Component({
   standalone: true,
-  imports: [CommonModule],
-  templateUrl: './change-detection.component.html',
-  styles: ``
+  imports: [TitleComponent, CommonModule],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  template: `
+    <app-title [title]='currentFramework()'></app-title>
+    <pre>As property: {{ frameworkAsProperty | json }}</pre>
+    <pre>As signal: {{ frameworkAsSignal() | json }}</pre>
+  `
 })
 export default class ChangeDetectionComponent {
+
+  public currentFramework = computed(
+    () => `Change Detection - ${this.frameworkAsSignal().name}`
+  );
+
+  public frameworkAsSignal = signal({
+    name:'Angular',
+    releaseDate: 2016
+  });
+  public frameworkAsProperty = {
+    name:'Angular',
+    releaseDate: 2016
+  };
+
+  constructor() {
+    setTimeout(() => {
+      // this.frameworkAsProperty.name = 'React'; Con ChangeDetectionStrategy.OnPush ya no funciona
+      this.frameworkAsSignal.update( value => ({
+        ...value,
+        name: 'Vue'
+      }))
+    }, 3000);
+  }
 
 }
