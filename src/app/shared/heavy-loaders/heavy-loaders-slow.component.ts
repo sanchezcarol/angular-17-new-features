cb0b54c0fb4efea70326007928247df31af9c3d3
Carol <carolsanchez071@gmail.com>
2024-02-26 12:25:45 -0400

    defer views

--- src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
+++ src/app/shared/heavy-loaders/heavy-loaders-slow.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { TitleComponent } from '../title/title.component';
 
 @Component({
   selector: 'app-heavy-loaders-slow',
   standalone: true,
-  imports: [],
-  template: ``,
+  imports: [CommonModule],
+  template: `
+    <section [ngClass]="['w-full h-[600px]', cssClass]">Hola Mundo</section>
+  `,
 })
 export class HeavyLoadersSlowComponent {
 
+  @Input({required: true}) cssClass!:string;
+
+    constructor() {
+      const start = Date.now();
+      while (Date.now() - start < 3000) {}
+    }
 }
